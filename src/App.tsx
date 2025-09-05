58f9a55ac97c74cdaef36259ad8c192fd0c9f8ce
ASM@GitHub2024 <asmaa14has@gmail.com>
2025-09-05 12:34:27 +0300

    create Product Card Copmonent

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import './App.css'
+import { ProductCard } from "./components/ProductCard"
 
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
     </>
   )
 }
---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,6 @@
+interface IProps{}
+export const ProductCard = ({}: IProps) => {
+  return (
+    <div>ProductCard</div>
+  )
+}
