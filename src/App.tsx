3d8abf048d3a8761925fb3b6751623b45f72e1e3
ASM@GitHub2024 <asmaa14has@gmail.com>
2025-09-05 13:49:18 +0300

    Product Card Style with Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,16 @@ function App() {
 
   return (
     <>
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
+      <div className="m-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-2">
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </div>
     </>
   )
 }
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,6 +1,29 @@
 interface IProps{}
 export const ProductCard = ({}: IProps) => {
   return (
-    <div>ProductCard</div>
+    <div className="border rounded-md p-2 flex flex-col">
+
+      <img src="https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?_gl=1*mcus9p*_ga*NjcxMTI4OTk0LjE3NTcwNjc5MjU.*_ga_8JE65Q40S6*czE3NTcwNjc5MjQkbzEkZzAkdDE3NTcwNjc5MjQkajYwJGwwJGgw" alt="prod_1"/>
+      
+      <h3> Product One For Sale</h3>
+      
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit ea explicabo necessitatibus doloremque.</p>
+
+      <div className="flex items-center space-x-2 my-2">
+        <span className="w-5 h-5 bg-red-400 rounded-full cursor-pointer"/>
+      <span className="w-5 h-5 bg-yellow-900 rounded-full cursor-pointer"/>
+      <span className="w-5 h-5 bg-blue-400 rounded-full cursor-pointer"/>
+      </div>
+
+      <div className="flex justify-between items-center mb-2">
+        <span>$500.000</span>
+      <img src="https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?_gl=1*mcus9p*_ga*NjcxMTI4OTk0LjE3NTcwNjc5MjU.*_ga_8JE65Q40S6*czE3NTcwNjc5MjQkbzEkZzAkdDE3NTcwNjc5MjQkajYwJGwwJGgw" alt="prod_1" className="w-10 h-10 rounded-full"/>
+      </div>
+
+      <div className="flex justify-between space-x-2 items-center mt-5">
+        <button className="bg-indigo-700 w-full text-white p-2 rounded-md">EDIT</button>
+        <button className="bg-red-700 w-full text-white p-2 rounded-md">DELETE</button>
+      </div>
+    </div>
   )
 }
