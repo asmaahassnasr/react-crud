8fe894e2b8695f984882150df3873b9bedfec966
ASM@GitHub2024 <asmaa14has@gmail.com>
2025-09-06 17:14:09 +0300

    make product card component more dynamic

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import { ProductCard } from "./components/ProductCard"
+import { productList } from "./data"
 
 function App() {
 
+  const renderedProductList = productList.map(prod => <ProductCard key={prod.id} product={prod} />)
   return (
     <>
+
       <div className="m-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-2">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        {renderedProductList}
       </div>
     </>
   )
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,28 +1,30 @@
-import {Image} from './Image'
+import type { IProduct } from '../interfaces'
+import { Image } from './Image'
 import Button from './UI/Button'
 
-interface IProps{}
+interface IProps {
+  product: IProduct
+}
 
-export const ProductCard = ({}: IProps) => {
+export const ProductCard = ({ product }: IProps) => {
+
+  const { description, imageURL, price, title } = product;
+
   return (
     <div className="border rounded-md p-2 flex flex-col">
-
-      <Image imageSrc="https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?_gl=1*mcus9p*_ga*NjcxMTI4OTk0LjE3NTcwNjc5MjU.*_ga_8JE65Q40S6*czE3NTcwNjc5MjQkbzEkZzAkdDE3NTcwNjc5MjQkajYwJGwwJGgw"
-      imageAlt="Product Name" imageClasses="rounded-md"/>
-      <h3> Product One For Sale</h3>
       
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit ea explicabo necessitatibus doloremque.</p>
-
+      <Image imageSrc={imageURL} imageAlt="Product Name" imageClasses="rounded-md" />
+      <h3>{title}</h3>
+      <p> {description} </p>
       <div className="flex items-center space-x-2 my-2">
-        <span className="w-5 h-5 bg-red-400 rounded-full cursor-pointer"/>
-      <span className="w-5 h-5 bg-yellow-900 rounded-full cursor-pointer"/>
-      <span className="w-5 h-5 bg-blue-400 rounded-full cursor-pointer"/>
+        <span className="w-5 h-5 bg-red-400 rounded-full cursor-pointer" />
+        <span className="w-5 h-5 bg-yellow-900 rounded-full cursor-pointer" />
+        <span className="w-5 h-5 bg-blue-400 rounded-full cursor-pointer" />
       </div>
 
       <div className="flex justify-between items-center mb-2">
-        <span>$500.000</span>
-      <Image imageSrc="https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?_gl=1*mcus9p*_ga*NjcxMTI4OTk0LjE3NTcwNjc5MjU.*_ga_8JE65Q40S6*czE3NTcwNjc5MjQkbzEkZzAkdDE3NTcwNjc5MjQkajYwJGwwJGgw"
-      imageAlt="Product Name" imageClasses="w-10 h-10 rounded-full object-center"/>
+        <span>${price}</span>
+        <Image imageSrc={imageURL} imageAlt="Product Name" imageClasses="w-10 h-10 rounded-full object-center" />
       </div>
 
       <div className="flex justify-between space-x-2 items-center mt-5">
