184af8819296e08f9f07e44bc84a0e7811332ed2
ASM@GitHub2024 <asmaa14has@gmail.com>
2025-09-12 08:15:38 +0300

    creat AddProduct form

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import { useState } from "react"
 import { ProductCard } from "./components/ProductCard"
 import Modal from "./components/UI/Modal"
-import { productList } from "./data"
+import { formInputsList, productList } from "./data"
 import Button from "./components/UI/Button"
+import Input from "./components/UI/Input"
 
 function App() {
 
   let [isOpen, setIsOpen] = useState(false)
+
+  const renderedProductList = productList.map(prod => <ProductCard key={prod.id} product={prod} />)
+
+  const renderFormInputsList = formInputsList.map(ele => 
+    <div className="flex flex-col">
+      <label className="mb-2 font-medium text-sm text-gray-700" htmlFor={ele.id}>{ele.label}</label>
+      <Input type={ele.type} name={ele.name} id={ele.id} />
+    </div>
+  )
 
   function open() {
     setIsOpen(true)
@@ -16,8 +26,7 @@ function App() {
     setIsOpen(false)
   }
 
-
-  const renderedProductList = productList.map(prod => <ProductCard key={prod.id} product={prod} />)
+  
   return (
     <>
       <main className="container mx-auto">
@@ -29,10 +38,13 @@ function App() {
         </div>
 
         <Modal isOpen={isOpen} title="Add New Product" closeModal={close}>
+          <form className="space-y-3">
+            {renderFormInputsList}
           <div className="flex items-center space-x-3">
             <Button className="bg-indigo-700 hover:bg-indigo-800">Submit</Button>
-            <Button className="bg-gray-300 hover:bg-gray-400" onClick={close}>Close</Button>
+            <Button className="bg-gray-500 hover:bg-gray-600" onClick={close}>Close</Button>
           </div>
+          </form>
         </Modal>
       </main>
     </>
--- src/components/UI/Modal.tsx
+++ src/components/UI/Modal.tsx
@@ -1,6 +1,5 @@
-import { Button, Dialog, DialogPanel, DialogTitle } from '@headlessui/react'
-import { useState, type ReactNode } from 'react'
-
+import {Dialog, DialogPanel, DialogTitle } from '@headlessui/react'
+import {type ReactNode } from 'react'
 
 interface IProps{
   isOpen:boolean;
@@ -17,7 +16,7 @@ const Modal = ({isOpen,closeModal,title,children} : IProps)=> {
           <div className="flex min-h-full items-center justify-center p-4">
             <DialogPanel
               transition
-              className="w-full max-w-md rounded-xl bg-white/5 p-6 backdrop-blur-2xl duration-300 ease-out data-closed:transform-[scale(95%)] data-closed:opacity-0"
+              className="w-full max-w-md rounded-xl bg-white p-6 duration-300 ease-out data-closed:transform-[scale(95%)] data-closed:opacity-0"
             >
               {title && <DialogTitle as="h3" className="text-base/7 font-medium">
                 {title}
