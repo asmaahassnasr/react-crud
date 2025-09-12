7d1ee3463602d37fc2f25fad00a6f793476d3ab1
ASM@GitHub2024 <asmaa14has@gmail.com>
2025-09-12 08:54:31 +0300

    enhance AddProduct Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,48 @@
-import { useState } from "react"
+import { useState, type ChangeEvent } from "react"
 import { ProductCard } from "./components/ProductCard"
 import Modal from "./components/UI/Modal"
 import { formInputsList, productList } from "./data"
 import Button from "./components/UI/Button"
 import Input from "./components/UI/Input"
+import type { IProduct } from "./interfaces"
 
 function App() {
 
   let [isOpen, setIsOpen] = useState(false)
 
-  const renderedProductList = productList.map(prod => <ProductCard key={prod.id} product={prod} />)
+  let [product, setProduct] = useState<IProduct>({
+    title: "",
+    price: "",
+    imageURL: "",
+    description: "",
+    colors: [],
+    category: {
+      name: "",
+      imageURL: ""
+    }
+  })
 
-  const renderFormInputsList = formInputsList.map(ele => 
-    <div className="flex flex-col">
+ 
+
+  const open = () => setIsOpen(true)
+  const close = () => setIsOpen(false)
+
+  const onChangeHandler = (evt: ChangeEvent<HTMLInputElement>) =>{
+    const {value, name} = evt.target;
+    setProduct({
+      ...product,
+      [name]:value
+    })
+  }
+
+ const renderedProductList = productList.map(prod => <ProductCard key={prod.id} product={prod} />)
+
+  const renderFormInputsList = formInputsList.map(ele =>
+    <div className="flex flex-col" key={ele.id}>
       <label className="mb-2 font-medium text-sm text-gray-700" htmlFor={ele.id}>{ele.label}</label>
-      <Input type={ele.type} name={ele.name} id={ele.id} />
+      <Input type={ele.type} name={ele.name} id={ele.id} value={product[ele.name]} onChange={onChangeHandler} />
     </div>
   )
-
-  function open() {
-    setIsOpen(true)
-  }
-
-  function close() {
-    setIsOpen(false)
-  }
-
-  
   return (
     <>
       <main className="container mx-auto">
@@ -40,10 +56,10 @@ function App() {
         <Modal isOpen={isOpen} title="Add New Product" closeModal={close}>
           <form className="space-y-3">
             {renderFormInputsList}
-          <div className="flex items-center space-x-3">
-            <Button className="bg-indigo-700 hover:bg-indigo-800">Submit</Button>
-            <Button className="bg-gray-500 hover:bg-gray-600" onClick={close}>Close</Button>
-          </div>
+            <div className="flex items-center space-x-3">
+              <Button className="bg-indigo-700 hover:bg-indigo-800">Submit</Button>
+              <Button className="bg-gray-500 hover:bg-gray-600" onClick={close}>Close</Button>
+            </div>
           </form>
         </Modal>
       </main>
